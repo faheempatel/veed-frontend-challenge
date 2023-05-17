aa4247d23022ea37ad76f75119a5591436c4bf60
Faheem Patel <faheem@faheempatel.com>
2023-05-17 15:37:40 +0300

    Add basic favouriting functionality

--- src/app/repoList.tsx
+++ src/app/repoList.tsx
@@ -1,11 +1,43 @@
 'use client';
 
+import { useState } from 'react';
+import { FaBookmark, FaRegBookmark } from 'react-icons/fa';
+import classNames from 'classnames';
+
 const RepoList = ({ repos }) => {
+  const [favourites, setFavourites] = useState<number[]>([]);
+
+  const favouriteHandler = (id: number) => () => {
+    if (favourites.includes(id)) {
+      setFavourites(favourites.filter((item) => item !== id));
+    } else {
+      setFavourites([...favourites, id]);
+    }
+  };
+
   return (
     <>
       {repos.map((repo) => {
+        const favourited = favourites.includes(repo.id);
         return (
-          <div key={repo.id}>
+          <div
+            key={repo.id}
+            className={classNames('relative p-[16px] rounded-md', {
+              [favourited ? 'bg-yellow-500 text-yellow-900' : 'bg-[#1e1a25]']:
+                true,
+            })}
+          >
+            <div
+              className="cursor-pointer absolute  top-[16px] right-[16px]"
+              onClick={favouriteHandler(repo.id)}
+            >
+              {favourited ? (
+                <FaBookmark className="w-[16px] h-[16px]" />
+              ) : (
+                <FaRegBookmark className="w-[16px] h-[16px]" />
+              )}
+            </div>
+
             <div className="space-y-[8px] leading-tight">
               <p className="font-bold">{repo.full_name}</p>
               <p className="max-w-prose">{repo.description}</p>
