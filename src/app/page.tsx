ed81b1c66bc5532d71c92938eeb1b7d6d3165fb9
Faheem Patel <faheem@faheempatel.com>
2023-05-17 15:17:21 +0300

    Query repos from github api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,32 @@
+const fetchPopularRepositories = async ({
+  createdDaysAgo = 7,
+  sort = 'stars',
+  order = 'desc',
+  perPage = 10,
+} = {}) => {
+  const apiUrl = 'https://api.github.com/search/repositories';
+
+  const createdDate = new Date();
+  createdDate.setDate(createdDate.getDate() - createdDaysAgo);
+  const createdDateString = createdDate.toISOString().split('T')[0]; // Format date as YYYY-MM-DD
+
+  const query = `created:>${createdDateString}`;
+
+  const url = `${apiUrl}?q=${query}&sort=${sort}&order=${order}&per_page=${perPage}`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (response.ok) {
+    return data.items;
+  } else {
+    throw new Error(data.message);
+  }
+};
+
 const Home = async () => {
+  const repos = await fetchPopularRepositories();
+  console.log(repos);
+
   return (
     <main className="">
       <h1 className="">
@@ -6,6 +34,21 @@ const Home = async () => {
         <br /> repos
       </h1>
       <p className="">Most starred repos created in the last 7 days</p>
+      {repos.map((repo) => {
+        return (
+          <div key={repo.id}>
+            <div>
+              <p>{repo.full_name}</p>
+              <p>{repo.description}</p>
+              <p>{repo.stargazers_count}</p>
+              {repo.language && <p>{repo.language}</p>}
+              <a href={repo.html_url}>
+                <p>Link to repo</p>
+              </a>
+            </div>
+          </div>
+        );
+      })}
     </main>
   );
 };
